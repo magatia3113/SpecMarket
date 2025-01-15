c829f8c0ac61088532e949ad4bcb9dd70fa33a42
boomboomshakeit <lth069389@naver.com>
2025-01-15 14:09:35 +0900

    springboot

--- specmarket/src/main/java/com/example/specmarket/SpecmarketApplication.java
+++ specmarket/src/main/java/com/example/specmarket/SpecmarketApplication.java
@@ -1,13 +1,13 @@
-package com.example.shop;
+package com.example.specmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShopApplication {
+public class SpecmarketApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ShopApplication.class, args);
+		SpringApplication.run(SpecmarketApplication.class, args);
 	}
 
 }
--- specmarket/src/test/java/com/example/specmarket/SpecmarketApplicationTests.java
+++ specmarket/src/test/java/com/example/specmarket/SpecmarketApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.shop;
+package com.example.specmarket;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ShopApplicationTests {
+class SpecmarketApplicationTests {
 
 	@Test
 	void contextLoads() {
