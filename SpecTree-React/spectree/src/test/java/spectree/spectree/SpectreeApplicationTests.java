b2656a2bd5d9b8500d2b81256b303183e56a80fe
magatia3113 <116639627+magatia3113@users.noreply.github.com>
2025-01-25 04:33:35 +0900

    React 프로젝트 구성

---
+++ SpecTree-React/spectree/src/test/java/spectree/spectree/SpectreeApplicationTests.java
@@ -0,0 +1,13 @@
+package spectree.spectree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpectreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
